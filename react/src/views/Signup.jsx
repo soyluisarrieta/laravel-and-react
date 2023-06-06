79ffca347ee3952a44fd3c7f66a592a82eab6dd9
Luis Arrieta <luisarrieta796@gmail.com>
2023-06-06 16:27:23 -0500

    Create Signup interface

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,7 +1,25 @@
+import { Link } from 'react-router-dom'
+
 export default function Signup () {
+  const onSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <>
-      <h1>Signup</h1>
-    </>
+    <div className='login-signup-form animated fadeInDown'>
+      <div className='form'>
+        <form onSubmit={onSubmit}>
+          <h1 className='title'>Login into your account</h1>
+          <input type='text' placeholder='Full name' />
+          <input type='email' placeholder='Email' />
+          <input type='password' placeholder='Password' />
+          <input type='password' placeholder='Password Confirmation' />
+          <button className='btn btn-block'>Signup</button>
+          <p className='message'>
+            Already registered? <Link to='/login'>Sign in</Link>
+          </p>
+        </form>
+      </div>
+    </div>
   )
 }
