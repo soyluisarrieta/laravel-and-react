e9f04ed55b02c655e820acefb324b502e079d2dc
Luis Arrieta <luisarrieta796@gmail.com>
2023-06-06 17:42:20 -0500

    Create and return user and token generated in the controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,15 +5,28 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-  public function login(LoginRequest $request)
-  {
-  }
 
   public function signup(SignupRequest $request)
+  {
+    $data = $request->validated();
+    /** @var \App\Models\User $user */
+    $user = User::create([
+      'name' => $data['name'],
+      'email' => $data['email'],
+      'password' => bcrypt($data['password'])
+    ]);
+
+    $token = $user->createToken('main')->plainTextToken;
+
+    return response(compact('user', 'token'));
+  }
+
+  public function login(LoginRequest $request)
   {
   }
 
