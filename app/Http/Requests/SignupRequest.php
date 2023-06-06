42865eff649408e84594abb86333401dbfaa8ded
Luis Arrieta <luisarrieta796@gmail.com>
2023-06-06 17:54:26 -0500

    Add rules for form data validate in Signup

--- app/Http/Requests/SignupRequest.php
+++ app/Http/Requests/SignupRequest.php
@@ -3,28 +3,37 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Password;
 
 class SignupRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return false;
-    }
+  /**
+   * Determine if the user is authorized to make this request.
+   *
+   * @return bool
+   */
+  public function authorize()
+  {
+    return true;
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, mixed>
-     */
-    public function rules()
-    {
-        return [
-            //
-        ];
-    }
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, mixed>
+   */
+  public function rules()
+  {
+    return [
+      'name' => 'required|string|max:55',
+      'email' => 'required|email|unique:users,email',
+      'passowrd' => [
+        'required',
+        'confirmed',
+        Password::min(8)
+          ->letters()
+          ->symbols()
+      ]
+    ];
+  }
 }
