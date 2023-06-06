f0b4e49bc48e4a6a8c5af8ddc109d31cd5712f0a
Luis Arrieta <luisarrieta796@gmail.com>
2023-06-06 17:13:39 -0500

    Create the Requests

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,19 +3,21 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-  public function login()
+  public function login(LoginRequest $request)
   {
   }
 
-  public function signup()
+  public function signup(SignupRequest $request)
   {
   }
 
-  public function logout()
+  public function logout(Request $request)
   {
   }
 }
