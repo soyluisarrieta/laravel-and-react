7d2648ae019ca5b417005fad790a560375a55abe
Luis Arrieta <luisarrieta796@gmail.com>
2023-06-06 16:56:51 -0500

    Get form data signup usign useRef

--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,8 +1,22 @@
+import { useRef } from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Signup () {
+  const nameRef = useRef()
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const passwordConfirmationRef = useRef()
+
   const onSubmit = (e) => {
     e.preventDefault()
+    const payload = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: passwordConfirmationRef.current.value
+    }
+
+    console.log({ payload })
   }
 
   return (
@@ -10,10 +24,10 @@ export default function Signup () {
       <div className='form'>
         <form onSubmit={onSubmit}>
           <h1 className='title'>Login into your account</h1>
-          <input type='text' placeholder='Full name' />
-          <input type='email' placeholder='Email' />
-          <input type='password' placeholder='Password' />
-          <input type='password' placeholder='Password Confirmation' />
+          <input ref={nameRef} type='text' placeholder='Full name' />
+          <input ref={emailRef} type='email' placeholder='Email' />
+          <input ref={passwordRef} type='password' placeholder='Password' />
+          <input ref={passwordConfirmationRef} type='password' placeholder='Password Confirmation' />
           <button className='btn btn-block'>Signup</button>
           <p className='message'>
             Already registered? <Link to='/login'>Sign in</Link>
