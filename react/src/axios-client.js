453eb1678a1e1d49eb6b16b4f48cd1dc11d57236
Luis Arrieta <luisarrieta796@gmail.com>
2023-06-06 19:01:20 -0500

    Fix Base URL and getItem

--- react/src/axios-client.js
+++ react/src/axios-client.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 const axiosClient = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`
 })
 
 axiosClient.interceptors.request.use((config) => {
-  const token = window.localStorage.get('ACCESS_TOKEN')
+  const token = window.localStorage.getItem('ACCESS_TOKEN')
   config.headers.Authorization = `Bearer ${token}`
   return config
 })
@@ -13,9 +13,13 @@ axiosClient.interceptors.request.use((config) => {
 axiosClient.interceptors.response.use((response) => {
   return response
 }, (error) => {
-  const { response } = error
-  if (response.status === 401) {
-    window.localStorage.removeItem('ACCESS_TOKEN')
+  try {
+    const { response } = error
+    if (response.status === 401) {
+      window.localStorage.removeItem('ACCESS_TOKEN')
+    }
+  } catch (err) {
+    console.error(err)
   }
 
   throw error
