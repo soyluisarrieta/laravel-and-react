ccf96d4cdd75f97a222017531cd16af0f9493e49
Luis Arrieta <luisarrieta796@gmail.com>
2023-06-06 20:59:00 -0500

    Finish Login sistem with the logout

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,8 +1,10 @@
 import { Link, Navigate, Outlet } from 'react-router-dom'
 import { useStateContext } from '../contexts/ContextProvider'
+import { useEffect } from 'react'
+import axiosClient from '../axios-client.js'
 
 export default function DefaultLayout () {
-  const { user, token } = useStateContext()
+  const { user, token, setUser, setToken } = useStateContext()
 
   if (!token) {
     return <Navigate to='/login' />
@@ -10,7 +12,20 @@ export default function DefaultLayout () {
 
   const onLogout = (e) => {
     e.preventDefault()
+
+    axiosClient.post('/logout')
+      .then(() => {
+        setUser({})
+        setToken(null)
+      })
   }
+
+  useEffect(() => {
+    axiosClient.get('/user')
+      .then(({ data }) => {
+        setUser(data)
+      })
+  }, [])
 
   return (
     <div id='defaultLayout'>
