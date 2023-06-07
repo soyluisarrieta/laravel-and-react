bc441c02c8ec0a03cb9e0e2c58ff85e81d194389
Luis Arrieta <luisarrieta796@gmail.com>
2023-06-06 19:04:00 -0500

    Fix typo and dependence in API

--- app/Http/Requests/SignupRequest.php
+++ app/Http/Requests/SignupRequest.php
@@ -3,7 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Support\Facades\Password;
+use Illuminate\Validation\Rules\Password;
 
 class SignupRequest extends FormRequest
 {
@@ -25,14 +25,15 @@ class SignupRequest extends FormRequest
   public function rules()
   {
     return [
-      'name' => 'required|string|max:55',
-      'email' => 'required|email|unique:users,email',
-      'passowrd' => [
+      'name' => ['required', 'string'],
+      'email' => ['required', 'email', 'unique:users,email'],
+      'password' => [
         'required',
         'confirmed',
         Password::min(8)
           ->letters()
           ->symbols()
+          ->numbers()
       ]
     ];
   }
