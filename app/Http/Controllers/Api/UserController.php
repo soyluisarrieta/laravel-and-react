62e56d3d5689f1bbafa202d4b8320d526b4ca43a
Luis Arrieta <luisarrieta796@gmail.com>
2023-06-07 12:34:09 -0500

    Update user data usign update method from User controller

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -56,7 +56,12 @@ class UserController extends Controller
    */
   public function update(UpdateUserRequest $request, User $user)
   {
-    //
+    $data = $request->validated();
+    if (isset($data['password'])) {
+      $data['password'] = bcrypt($data['password']);
+    }
+    $user->update($data);
+    return new UserResource($user);
   }
 
   /**
