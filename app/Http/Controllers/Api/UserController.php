eaffb276a61ad0a8b736d61bbd81e47f531d8d16
Luis Arrieta <luisarrieta796@gmail.com>
2023-06-07 12:30:41 -0500

    Create new user usign store method from User controller

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -30,7 +30,10 @@ class UserController extends Controller
    */
   public function store(StoreUserRequest $request)
   {
-    //
+    $data = $request->validated();
+    $data['password'] = bcrypt($data['password']);
+    $user = User::create($data);
+    return response(new UserResource($user), 201);
   }
 
   /**
