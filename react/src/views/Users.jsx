89b1dc8436aa2f77c8f499c9b4b68a218230f50f
Luis Arrieta <luisarrieta796@gmail.com>
2023-06-07 12:15:41 -0500

    Request users data from react

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,4 +1,25 @@
+import { useEffect, useState } from 'react'
+import axiosClient from '../axios-client.js'
+
 export default function Users () {
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  const getUsers = () => {
+    axiosClient.get('/users')
+      .then(({ data }) => {
+        setLoading(true)
+        console.log({ data })
+      })
+      .catch(() => {
+        setLoading(false)
+      })
+  }
+
   return (
     <>
       <h1>Users</h1>
