16390c897b9fbd23834d8b00c555a85a9fedbd97
Luis Arrieta <luisarrieta796@gmail.com>
2023-06-08 12:05:00 -0500

    Get user data usign useParams custom hook of react-router-dom

--- react/src/views/UserForm.jsx
+++ react/src/views/UserForm.jsx
@@ -1,4 +1,31 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axiosClient from '../axios-client.js'
+
 export default function UserForm () {
+  const { id } = useParams()
+  const [loading, setLoading] = useState(false)
+  const [user, setUser] = useState({
+    id: null,
+    name: '',
+    email: '',
+    password: '',
+    password_confirmation: ''
+  })
+
+  useEffect(() => {
+    if (!id) return
+
+    setLoading(true)
+    axiosClient.get(`/users/${id}`)
+      .then(({ data }) => {
+        setUser(data)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
   return (
     <div>
       UserForm
