8909f735fb15ea01c6613a3b2e8b7c16707898f1
Luis Arrieta <luisarrieta796@gmail.com>
2023-06-08 11:22:00 -0500

    Implement table for renderize users

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -21,11 +21,42 @@ export default function Users () {
       })
   }
 
+  const handleDelete = () => {
+
+  }
+
   return (
     <div>
       <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
         <h1>Users</h1>
         <Link to='/users/new' className='btn-add'>Add new</Link>
+      </div>
+      <div className='card animated fadeInDown'>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Create Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(({ id, name, email, created_at: createAt }) => (
+              <tr key={id}>
+                <td>{id}</td>
+                <td>{name}</td>
+                <td>{email}</td>
+                <td>{createAt}</td>
+                <td>
+                  <Link to={`/users/${id}`}>Edit</Link>
+                  <button onClick={handleDelete} className='btn-delete'>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   )
