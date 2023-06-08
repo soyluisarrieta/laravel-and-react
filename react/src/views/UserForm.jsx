5e8b9da35dfded07d670ea3b7a4e2c6cc9d9e6ec
Luis Arrieta <luisarrieta796@gmail.com>
2023-06-08 12:28:11 -0500

    Show user data in the form for update

--- react/src/views/UserForm.jsx
+++ react/src/views/UserForm.jsx
@@ -5,6 +5,7 @@ import axiosClient from '../axios-client.js'
 export default function UserForm () {
   const { id } = useParams()
   const [loading, setLoading] = useState(false)
+  const [errors, setErrors] = useState(false)
   const [user, setUser] = useState({
     id: null,
     name: '',
@@ -26,9 +27,58 @@ export default function UserForm () {
       })
   }, [])
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      UserForm
-    </div>
+    <>
+      <h1>
+        {user.id
+          ? `Update User: ${user.name}`
+          : 'New User'}
+      </h1>
+      <div className='card animated fadeInDown'>
+        {loading && (
+          <div className='text-center'>Loading...</div>
+        )}
+
+        {errors && (
+          <div className='alert'>
+            {Object.keys(errors).map(key => (
+              <p key={key}>{errors[key][0]}</p>
+            ))}
+          </div>
+        )}
+
+        {!loading && (
+          <form onSubmit={handleSubmit}>
+            <input
+              onChange={({ target }) => setUser({ ...user, name: target.value })}
+              value={user.name}
+              type='text'
+              placeholder='Name'
+            />
+            <input
+              onChange={({ target }) => setUser({ ...user, email: target.value })}
+              value={user.email}
+              type='email'
+              placeholder='Email'
+            />
+            <input
+              onChange={({ target }) => setUser({ ...user, password: target.value })}
+              type='password'
+              placeholder='Password'
+            />
+            <input
+              onChange={({ target }) => setUser({ ...user, password_confirmation: target.value })}
+              type='password'
+              placeholder='Password Confirmation'
+            />
+            <button type='submit' className='btn'>Save</button>
+          </form>
+        )}
+      </div>
+    </>
   )
 }
